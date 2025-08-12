675bb206ea5678cf3154022a6cf937162c406f68
diegoselva7x <diegoselva7x@gmail.com>
2025-08-11 19:38:13 -0800

    Fix linting error: remove unused isVisible variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 import Hero from '@/features/common/landing/content/hero';
 import { FeaturesShowcase } from '@/features/common/landing/content/features-showcase/FeaturesShowcase';
+import { NetworkStats } from '@/features/common/landing/content';
 
 export default function LandingPage() {
   return (
     <main className='flex flex-col justify-center items-center mt-[7rem]'>
       <Hero />
       <FeaturesShowcase />
+      <NetworkStats />
     </main>
   );
 }
